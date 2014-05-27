7af81fbda3bfdea7553a87ed47e4a404d2e9ea4a
ToTea <koniarone445440@gmail.com>
2014-05-28 01:50:17 +0800

    Modified response

--- public/assets/js/event.js
+++ public/assets/js/event.js
@@ -6,9 +6,14 @@ var tmpl = '<li><span></span></li>',
 
 commentInput.on("keyup", function(e){
 	if(e.which === 13){
-		var input = $(this), ul = input.parents(".aspect_comment").find(".comment_list");
+		var input = $(this),
+			ul = input.parents(".aspect_comment").find(".comment_list"),
+			scrollTo;
 
 		$(tmpl).appendTo(ul).find("span").text(input.val());
+		input.val("");
+		scrollTo = ul.find("li:last");
+		ul.scrollTop(scrollTo.offset().top - ul.offset().top + ul.scrollTop());
 	}
 })
 
