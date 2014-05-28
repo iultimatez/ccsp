41b64784c18f60e1dce14ddd4bdd470c863d937d
ToTea <koniarone445440@gmail.com>
2014-05-28 10:47:50 +0800

    Add more dimension

--- public/assets/js/event.js
+++ public/assets/js/event.js
@@ -1,6 +1,7 @@
 (function(){
 	
-var tmpl = '<li><span></span></li>',
+//add new comment
+var commentTmpl = '<li><span></span></li>',
 	commentInput = $(".comment_input");
 
 
@@ -10,12 +11,55 @@ commentInput.on("keyup", function(e){
 			ul = input.parents(".aspect_comment").find(".comment_list"),
 			scrollTo;
 
-		$(tmpl).appendTo(ul).find("span").text(input.val());
+		$(commentTmpl).appendTo(ul).find("span").text(input.val());
 		input.val("");
 		scrollTo = ul.find("li:last");
 		ul.scrollTop(scrollTo.offset().top - ul.offset().top + ul.scrollTop());
 	}
 })
 
+//load dimension
+var dimensions = {"topicId": "", "dimension": []};
+var dimension = {"dimensionId": "", "name": "", "comment": []};
+
+dimension.dimensionId = "0";
+dimension.name = "TopicName";
+dimension.comment.push("123");
+dimensions.topicId = "0";
+dimensions.dimension.push(dimension);
+
+var emptyD = $(".isEmpty:first");
+
+function loadDimension(){
+	var ul = $(emptyD).find("ul");
+
+	$(emptyD).find("p").text(dimension.name);
+	$(commentTmpl).appendTo(ul).find("span").text(dimension.comment.pop());
+
+	$(emptyD).removeClass("isEmpty");
+}
+
+loadDimension();
+
+//add dimension
+var more = $("#hexagon");
+
+more.on("click", function(){
+	var emptyD = $(".isEmpty:first");
+	var aspect = $(emptyD).find(".aspect_col");
+	var titleInput = $(aspect).addClass("is-editing").find(".aspect_title input");
+
+	$(emptyD).removeClass("isEmpty");
+	$(titleInput).focus();
+
+	titleInput.on("keyup", function(e){
+		if(e.which === 13){
+			var input = $(this);
+
+			$(aspect).find("p").text(input.val());
+			$(aspect).removeClass("is-editing");
+		}
+	})
+})
 
 }());
