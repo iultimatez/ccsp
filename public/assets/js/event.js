82d53db9b555827e0dd8574ce7cbd6248ab3eae7
iultimatez <appledynasty@gmail.com>
2014-05-29 00:16:50 +0800

    add modal

--- public/assets/js/event.js
+++ public/assets/js/event.js
@@ -1,15 +1,35 @@
 (function(){
 	
 var tmpl = '<li><span></span></li>',
-	commentInput = $(".comment_input");
+	commentInput = $(".comment_input"),
+	commentInput2 = $(".comment_input2"),
+	commentInput3 = $(".submit_comment");
 
 
 commentInput.on("keyup", function(e){
 	if(e.which === 13){
 		var input = $(this), ul = input.parents(".aspect_comment").find(".comment_list");
-
+		var expand = input.parents(".aspect_col").find(".aspect_title").find(".comment_list");
 		$(tmpl).appendTo(ul).find("span").text(input.val());
+		$(tmpl).appendTo(expand).find("span").text(input.val());
 	}
+})
+
+commentInput2.on("keyup", function(e){
+	if(e.which === 13){
+		var input = $(this), ul = input.parents(".aspect_col").find(".aspect_comment").find(".comment_list");
+		var expand = input.parents(".modal-content").find(".comment_list");
+		$(tmpl).appendTo(ul).find("span").text(input.val());
+		$(tmpl).appendTo(expand).find("span").text(input.val());
+	}
+})
+
+commentInput3.on("click", function(e){
+	var input = $(this), ul = input.parents(".aspect_col").find(".aspect_comment").find(".comment_list");
+	var expand = input.parents(".modal-content").find(".comment_list");
+	var inputValue = $(this).siblings(".col-xs-10").find(".comment_input2");
+	$(tmpl).appendTo(ul).find("span").text(inputValue.val());
+	$(tmpl).appendTo(expand).find("span").text(inputValue.val());
 })
 
 
